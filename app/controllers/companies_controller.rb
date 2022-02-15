6f1ec77ca14022463cfb29cd59cbb8afc7a95a65
Louis <louis.harel@kellogg.northwestern.edu>
2022-02-15 16:16:40 +0000

    created show action

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,11 +2,13 @@ class CompaniesController < ApplicationController
 
   def index
     @companies = Company.all
+    #@contacts = Contact.all
   end
 
   def show
     # .find(123) is the same as .where({ id: 123 })[0]
     @company = Company.find(params["id"])
+    #@contact = Contact.find(params["id"])
   end
 
 end
